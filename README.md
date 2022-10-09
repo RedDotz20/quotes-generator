0dd3f517954aad2d6fbc43fbd20de17c9026b361
Carlos Tabangay <carlos_tabangay@outlook.com>
2022-10-09 23:23:22 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# React-App-Template with Vite
+
+### Getting Started
+```
+cd root
+npm install
+npm run dev
+```
---
+++ root/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+	return (
+		<div className="App">
+			<h1>React App</h1>
+		</div>
+	);
+}
