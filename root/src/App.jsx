0f33afb66f4343b5bcf73fe7eb6dadc64819a4e6
RedDotz20 <carlos_tabangay@outlook.com>
2022-12-05 00:53:01 +0800

    refactor: react query modified

--- root/src/App.jsx
+++ root/src/App.jsx
@@ -1,41 +1,43 @@
-import { useQuery } from "react-query";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Button from "@mui/material/Button";
 import "./App.css";
 import "./index.css";
 
-async function fetchQuotes() {
+async function fetchAxiosQuotes() {
 	return await axios.get("https://type.fit/api/quotes").then((res) => res.data);
 }
 
 export default function App() {
-	const {
-		data: quotes,
-		isLoading,
-		isError,
-		error,
-		refetch,
-	} = useQuery(["quotes"], fetchQuotes);
-
-	const randomId = Math.floor(Math.random() * quotes?.length);
-
-	if (isLoading)
+	const { status, data, error, refetch } = useQuery({
+		queryKey: ["quotes"],
+		queryFn: fetchAxiosQuotes,
+	});
+	if (status === "loading")
 		return (
 			<div className="flex flex-col justify-center text-center">
 				<div className="animate-spin bg-transparent h-4 sw-4 rounded-full border-t-2 border-blue-900"></div>
 				<h2>Loading...</h2>
 			</div>
 		);
+	if (status === "error") return `An error has occurred: ${error.message}`;
 
-	if (isError) return `An error has occurred: ${error.message}`;
+	const randomId = Math.floor(Math.random() * data?.length);
 
+	console.log(data[randomId]);
 	return (
 		<>
 			<div className="bg-white rounded-lg text-black p-6">
 				<h1>QUOTES GENERATOR:</h1>
 				<div>
-					<p>{quotes[randomId]?.text}</p>
-					<p>- {quotes[randomId]?.author}</p>
+					<p>"{data[randomId].text}"</p>
+					<p>
+						<i>
+							{data[randomId].author === null
+								? "- Unknown"
+								: `- ${data[randomId].author}`}
+						</i>
+					</p>
 				</div>
 			</div>
 
--- root/src/main.jsx
+++ root/src/main.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: { refetchOnWindowFocus: false, refetchOnMount: false },
-	},
+	// defaultOptions: {
+	// 	queries: { refetchOnWindowFocus: false, refetchOnMount: false },
+	// },
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
 			<App />
+			<ReactQueryDevtools initialIsOpen={true} />
 		</QueryClientProvider>
 	</React.StrictMode>
 );
