3c8f3fd7bc42f545a556da19f90cd01d8458fc5f
RedDotz20 <carlos_tabangay@outlook.com>
2023-01-12 19:09:13 +0800

    styles: button change

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -5,16 +5,12 @@ import { useQuery } from "react-query";
 export default function QueryBtn({ setRandomId }) {
 	const { data, isFetching } = useQuery(["quotes"]);
 	return (
-		<div className="bg-green-600/90 font-semibold text-white">
-			<Button
-				onClick={() => setRandomId(Math.floor(Math.random() * data?.length))}
-				className="w-full"
-				variant="contained"
-				color="success"
-				disabled={isFetching ? true : false}
-			>
-				{isFetching ? <Loading /> : "Generate New Random Quote"}
-			</Button>
-		</div>
+		<button
+			onClick={() => setRandomId(Math.floor(Math.random() * data?.length))}
+			disabled={isFetching ? true : false}
+			className="inline-block px-6 py-2 border-2 border-white text-white font-semibold text-xs leading-tight uppercase rounded hover:bg-black/[.75] focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-full tracking-widest"
+		>
+			Generate New Random Quote
+		</button>
 	);
 }
